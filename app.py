61e6804aaff507f4438f7ffa0ce3bb1328c11aa6
showall <gerardsho@gmail.com>
2023-04-23 22:34:09 +0800

    added an app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
 from subprocess import call
-
+from datetime import datetime, timedelta
+from pytz import timezone
+from dags.scrapyfbref.scrapyfbref.spiders.spider1_aws import MySpiderForPlayers
+import logging
 
 
 ######################Set up variables for use in our script
@@ -22,7 +25,7 @@ def scrape():
             pass
         a = os.getcwd()
         call(["scrapy", "crawl", "fbref","-s","CLOSESPIDER_PAGECOUNT=3"])
-        os.chdir('.')        
+        #os.chdir('.')        
         return (f"success")
 
 
