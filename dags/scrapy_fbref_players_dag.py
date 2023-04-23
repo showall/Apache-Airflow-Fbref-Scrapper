45faba8e38fab773482f89b8c1362c215f765185
showall <gerardsho@gmail.com>
2023-04-23 18:27:37 +0800

    cleared up local installation

--- dags/scrapy_fbref_players_dag.py
+++ dags/scrapy_fbref_players_dag.py
@@ -3,11 +3,20 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from subprocess import call
 import os
-from airflow.utils.dates import timezone
-from scrapyfbref.scrapyfbref.spiders.spider1 import MySpiderForPlayers
+from pytz import timezone
+from scrapyfbref.scrapyfbref.spiders.spider1_aws import MySpiderForPlayers
+#from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+import logging
+import os
 
-os.chdir('/home/gerardsho/airflow/dags/scrapyfbref/')
-local_tz = timezone.get_localzone()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+S3_BUCKET_NAME = 'fbrefdata0921'
+S3_KEY = 'output/fbref.csv'
+
+os.chdir('./dags/scrapyfbref/')
+local_tz = timezone('Asia/Singapore')
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -19,8 +28,23 @@ default_args = {
 dag = DAG('scrapy_fbref_players', default_args=default_args, schedule_interval=timedelta(days=1))
 
 def run_spider():
-    call(["scrapy", "crawl", "fbref"])
+  #  root_dir = os.path.abspath(os.path.join(os.sep, "var", "data", "webserver"))
+    logger.info(f"The first root directory is {os.getcwd()}.")
+    call(["scrapy", "crawl", "fbref","-s","CLOSESPIDER_PAGECOUNT=10"])
+    logger.info(f"The second root directory is {os.getcwd()}.")
+def upload_to_s3():
+  #  root_dir = os.path.abspath(os.path.join(os.sep, "var", "data", "webserver"))
+    logger.info(f"The root directory is {os.getcwd()}.")    
+    # s3 = S3Hook()
+    # s3.load_file(
+    #     filename='./scrapyfbref/file.csv',
+    #     key=S3_KEY,
+    #     bucket_name=S3_BUCKET_NAME,
+    #     replace=True
+    # )
+
 
 scrapy_task = PythonOperator(task_id='run_spider', python_callable=run_spider, dag=dag)
+load_file = PythonOperator(task_id='upload_to_s3', python_callable=upload_to_s3, dag=dag)
 
-
+scrapy_task >> load_file
