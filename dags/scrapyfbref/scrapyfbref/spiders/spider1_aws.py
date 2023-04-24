46e375bbef30459fcbbecbefc1c06fb7e1539e36
showall <gerardsho@gmail.com>
2023-04-25 02:20:08 +0800

    trial

--- dags/scrapyfbref/scrapyfbref/spiders/spider1_aws.py
+++ dags/scrapyfbref/scrapyfbref/spiders/spider1_aws.py
@@ -7,8 +7,8 @@ class MySpiderForPlayers(CrawlSpider):
     user_agent ="Custom"
     name = "fbref"
     allowed_domains = ["fbref.com"]
-    #start_urls =  ['https://fbref.com/en/players/']
-    start_urls = ["https://fbref.com/en/players/08f5afaa/Gianluigi-Donnarumma"]
+    start_urls =  ['https://fbref.com/en/players/']
+    #start_urls = ["https://fbref.com/en/players/08f5afaa/Gianluigi-Donnarumma"]
     custom_settings = {
         "DOWNLOAD_DELAY" : 4,
         "DOWNLOAD_TIMEOUT" : 9,
@@ -24,24 +24,40 @@ class MySpiderForPlayers(CrawlSpider):
 # }
     }    
     
+    # rules = (
+    #     Rule(LinkExtractor(allow = r'\/players\/([a-z]+)',
+    #                        restrict_xpaths=('//*[@class="section_wrapper"]')
+    #                        ), 
+    #         callback='parse_club_links', 
+    #         follow=True),
+    #     Rule(LinkExtractor(allow = r'\/players\/([a-z0-9]+)\/([A-z]+)',
+    #                                    restrict_xpaths='//*[@class="section_wrapper"]',
+    #                                     deny = r"\/players\/([a-z0-9]+)\/([A-z]+)\/\d+"
+    #                                    ), 
+    #         callback='parse_extract', follow=False),
+    # )
+
     rules = (
         Rule(LinkExtractor(allow = r'\/players\/([a-z]+)',
                            restrict_xpaths=('//*[@class="section_wrapper"]')
                            ), 
             callback='parse_club_links', 
-            follow=True),
+            follow=False),
         Rule(LinkExtractor(allow = r'\/players\/([a-z0-9]+)\/([A-z]+)',
                                        restrict_xpaths='//*[@class="section_wrapper"]',
-                                        deny = r"\/players\/([a-z0-9]+)\/([A-z]+)\/\d+"
+                                        deny = [ #r"\/players\/([a-z0-9]+)\/([A-z]+)\/\d+" ,
+                                               r"\/players\/([a-z0-9]+)\/([A-z]+)\/([A-z]+)" ]
                                        ), 
             callback='parse_extract', follow=False),
     )
-    
+
+
     def parse_club_links(self, response):
         count = 0 
         link_extractor = LinkExtractor(allow = r'\/players\/([a-z0-9]+)\/([A-z]+)',
                                        restrict_xpaths='//*[@class="section_wrapper"]',
-                                        deny = r"\/players\/([a-z0-9]+)\/([A-z]+)\/\d+"
+                                        deny = [ r"\/players\/([a-z0-9]+)\/([A-z]+)\/\d+" ,
+                                               r"\/players\/([a-z0-9]+)\/([A-z]+)\/([A-z]+)" ]
                                        )
         club_links = link_extractor.extract_links(response)
     #    print(response.url)
@@ -50,30 +66,22 @@ class MySpiderForPlayers(CrawlSpider):
 
         for link in club_links :
             #yield {"url": link.text, "text": link.url}
-          #  print(link.url)
-            print(link)
-            return scrapy.Request(link.url, callback=self.parse_extract, headers={'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'})
+            yield scrapy.Request(link.url, callback=self.parse_extract, headers={'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'})
 
 
     def parse_extract(self, response):
         print("HERE",response.url)    
         links = response.xpath('//*[(@class="listhead" and contains(., "Match Logs (Summary)"))]/following-sibling::*[1]//a/@href')
-
+        name = str(response.url).split("/")[-1]
         #links = LinkExtractor(restrict_xpaths='//*[(@class="listhead" and contains(text(), "Match Logs (Summary)"))]/following-sibling::*[1]')
       #  links = LinkExtractor()
       #  link_extractor_match_log = links.extract_links(response)    
-        for link in links:
+        for link in list(set(links)):
         #    yield scrapy.Request(response.urljoin(link), callback=self.parse_match_logs, headers={'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'})
             yield {
-           # "player_name": response.xpath('//h1/text()').get(),
-            "match_logs_url": response.url
+            "player_name": name,
+            "match_logs_url": link.url
              }
-
-
-
-
-
-
 
 
     # rules = (
